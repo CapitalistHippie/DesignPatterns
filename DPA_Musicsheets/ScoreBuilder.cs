dd640ec24dc443a61023917ef82a5cf9364b47db
mjowned <mj65@live.nl>
2016-09-12 14:19:14 +0200

    code for keycode, notesteps, alteration (sharps) and octaves. Not properly tested nor implemented tho

--- DPA_Musicsheets/ScoreBuilder.cs
+++ DPA_Musicsheets/ScoreBuilder.cs
@@ -11,6 +11,7 @@ namespace DPA_Musicsheets
     public class ScoreBuilder
     {
         private static ScoreBuilder instance;
+        private Dictionary<int, String> keycodeDictionary;
 
         public static ScoreBuilder Instance
         {
@@ -20,6 +21,25 @@ namespace DPA_Musicsheets
                     instance = new ScoreBuilder();
                 return instance;
             }
+        }
+
+        private ScoreBuilder() //this is correct right? private constructor
+        {
+            keycodeDictionary = new Dictionary<int, string>
+            {
+                {0, "C"},
+                {1, "C#"},
+                {2, "D"},
+                {3, "D#"},
+                {4, "E"},
+                {5, "F"},
+                {6, "F#"},
+                {7, "G"},
+                {8, "G#"},
+                {9, "A"},
+                {10, "A#"},
+                {11, "B"},
+            };
         }
 
         public Score BuildScoreFromMidi(String filePath)
@@ -50,6 +70,37 @@ namespace DPA_Musicsheets
                             // 160 is centrale C op piano.
                             //trackLog.Messages.Add(String.Format("Keycode: {0}, Command: {1}, absolute time: {2}, delta time: {3}"
                             //    , channelMessage.Data1, channelMessage.Command, midiEvent.AbsoluteTicks, midiEvent.DeltaTicks));
+
+
+                            // Get Note Step
+                            int keyCode = channelMessage.Data1;
+                            int keyCodeConverted = keyCode;
+                            while (keyCodeConverted > 11)
+                            {
+                                keyCodeConverted = keyCodeConverted - 12;
+                            }
+                            string noteStep = keycodeDictionary[keyCodeConverted];
+                            string cleanNoteStep = noteStep.TrimEnd('#');
+
+                            // Get Note Alter (Sharps)
+                            int alter = 0;
+                            if (noteStep.Contains("#"))
+                            {
+                                alter++;
+                            }
+
+                            // Get Note Octave
+                            int octave = 0;
+                            while (keyCode != keyCodeConverted)
+                            {
+                                keyCodeConverted = keyCodeConverted + 12;
+                                octave++;
+                            }
+
+                            // Get Note Duration
+
+                            // hmm not sure, will have to use deltaTime on the NoteOff probably
+
                             break;
                         case MessageType.SystemExclusive:
                             break;
