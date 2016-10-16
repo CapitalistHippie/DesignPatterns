a385f68468bf026076281f244a62ad37b8aaad4c
mjowned <mj65@live.nl>
2016-10-16 13:45:49 +0200

    MementoStatePattern thingy

---
+++ DPA_Musicsheets/Savepoints/SavePointKeeper.cs
@@ -0,0 +1,44 @@
+﻿using DPA_Musicsheets.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Savepoints
+{
+    public class SavepointKeeper
+    {
+        private Savepoint currentSavepoint;
+
+        public SavepointKeeper()
+        {
+
+        }
+
+        public void SetNewSavePoint(Score score, string lilyPondEditorText)
+        {
+            Savepoint newSavepoint = new Savepoint(score, lilyPondEditorText);
+            
+            if (currentSavepoint != null)
+            {
+                newSavepoint.OlderSavepoint = currentSavepoint;
+                currentSavepoint.NewerSavepoint = newSavepoint;
+            }
+
+            currentSavepoint = newSavepoint;
+        }
+
+        public Savepoint GetOlderSavepoint()
+        {
+            currentSavepoint = currentSavepoint.OlderSavepoint;
+            return currentSavepoint;
+        }
+
+        public Savepoint GetNewerSavepoint()
+        {
+            currentSavepoint = currentSavepoint.NewerSavepoint;
+            return currentSavepoint;
+        }
+    }
+}
---
+++ DPA_Musicsheets/Savepoints/Savepoint.cs
@@ -0,0 +1,24 @@
+﻿using DPA_Musicsheets.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Savepoints
+{
+    public class Savepoint
+    {
+        public Score score { get; private set; }
+        public string lilyPondEditorText { get; private set; }
+
+        public Savepoint OlderSavepoint { get; set; }
+        public Savepoint NewerSavepoint { get; set; }
+        
+        public Savepoint(Score score, string lilyPondEditorText)
+        {
+            this.score = score;
+            this.lilyPondEditorText = lilyPondEditorText;
+        }
+    }
+}
